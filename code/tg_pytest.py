57f35122196930bfdbf1b8b48c0b8fc3f14994d6
Ryan Sturmer <ryansturmer@gmail.com>
2016-07-12 14:41:10 -0400

    Some rearrangement of the main program to allow for running a specific test file instead of the whole suite

--- code/tg_pytest.py
+++ code/tg_pytest.py
@@ -8,7 +8,7 @@ __license__ = 'Python'
 
 #### CONSTANTS ####
 
-TEST_DATA_DIR = "../data"
+TEST_DATA_DIR = "../../data"
 TEST_MASTER_FILE = "test-master.cfg"
 OUTFILE_ENABLED = False     # True or False
 
@@ -23,6 +23,7 @@ import os
 from os.path import join
 from os.path import normpath
 from os.path import exists
+from os.path import abspath
 
 import time
 from time import sleep
@@ -30,6 +31,7 @@ from time import sleep
 import types
 
 from serial.tools.list_ports import comports
+import argparse
 
 # debugging assistants
 import inspect
@@ -69,7 +71,7 @@ def display_r(key, test_val, resp_val, response_string):
 #
 #   Analyzers
 #
-    
+
 def analyze_r(t_data, r_datae, params):
     """
     Analyze response objects in response list
@@ -89,13 +91,13 @@ def analyze_r(t_data, r_datae, params):
         if "response" in t_data["r"]:  # suppress the response
             if t_data["r"]["response"] == False:
                 r_data["response"] = ""
-                
+
         for key in t_data["r"]:
             if key in r_data["r"]:
 
                 test_val = t_data["r"][key]    # data value to check from test data
                 resp_val = r_data["r"][key]    # data value returned for this key from response
-                
+
                 # display nested responses
                 if isinstance(test_val, dict):
                     for child in test_val:
@@ -106,13 +108,13 @@ def analyze_r(t_data, r_datae, params):
                             print("  MISSING: \"{0}\" is missing from response".format(child))
                             result -= 1
                     continue
-                
+
                 # display non-nested responses
                 result -= display_r(key, test_val, resp_val, r_data["response"])
             else:
                 print("  MISSING: \"{0}\" is missing from response {1}".format(key, r_data["response"]))
                 result -= 1
-                
+
     return result
 
 
@@ -137,7 +139,7 @@ def analyze_sr(t_data, r_datae, params):
 
     print last_sr["response"]       # print response just once for all SR tests
 
-    # test if keys are present and match t_data           
+    # test if keys are present and match t_data
     for k in t_data["sr"]:
         if k in build_sr:
             if t_data["sr"][k] == build_sr[k]:
@@ -187,19 +189,19 @@ def send_before_after(key, data, delay):
 
     # Send the before/after strings
     if key == "before" and "before" in data:
-        send = [x.encode("utf8") for x in data["before"]]   
+        send = [x.encode("utf8") for x in data["before"]]
         for line in send:
             print("  before: {0}".format(line))
             tg.write(line+"\n")
             time.sleep(delay)
 
     if key == "after" and "after" in data:
-        send = [x.encode("utf8") for x in data["after"]]   
+        send = [x.encode("utf8") for x in data["after"]]
         for line in send:
             print("  after:  {0}".format(line))
             tg.write(line+"\n")
             time.sleep(delay)
-    
+
     responses = tg.readlines()       # collect all output before returning
 
 
@@ -208,7 +210,7 @@ def do_before_after(key, data, params):
     key == "before_all", "after_all", "before_each" or "after_each"
     data == dictionary nested under the above key
     """
-    
+
     if key not in data:             # silent return is OK
         return;
 
@@ -221,7 +223,7 @@ def do_before_after(key, data, params):
 
     if key == "after_each" and "after" in data[key]:
         send_before_after("after", data["after_each"], delay)
-        
+
     if key == "before_all" and "before" in data[key]:
         if "label" in data[key]:
             print
@@ -230,7 +232,7 @@ def do_before_after(key, data, params):
         tg.write("M2\n")            # end any motion and clear any alarms
         tg.write("{clear:null}\n")  # clear any alarms
         send_before_after("before", data["before_all"], delay)
-    
+
     if key == "after_all" and "after" in data[key]:
         if "label" in data[key]:
             print
@@ -273,9 +275,9 @@ def run_test(t_data, before_data, after_data, params):
     if "send" not in t_data["t"]:
         print("!!! TEST HAS NO SEND DATA: {0}".format(t_data["t"]["label"]))
         return
-    
+
     send = [x.encode("utf8") for x in t_data["t"]["send"]]
-    first_line = send[0]                # used later in no-response cases    
+    first_line = send[0]                # used later in no-response cases
     for line in send:
         print("  sending: {0}".format(line))
         tg.write(line+"\n")
@@ -295,7 +297,7 @@ def run_test(t_data, before_data, after_data, params):
         r_datae[-1]["response"] = line      # Add the response line to the dictionary
 
         if "r" in r_datae[-1]:
-            r_datae[-1]["r"]["status"] = r_datae[-1]['f'][1]  # extract status code from footer              
+            r_datae[-1]["r"]["status"] = r_datae[-1]['f'][1]  # extract status code from footer
             r_datae[-1]["r"]["count"] = r_datae[-1]['f'][2]   # extract byte/line count from footer
 
     if len(r_datae) == 0:
@@ -316,6 +318,22 @@ def run_test(t_data, before_data, after_data, params):
         send_before_after("after", t_data["t"], delay)      # local after's first
         do_before_after("after_each", after_data, params)
 
+def get_tests_from_file(filename):
+    with open(TEST_MASTER_FILE, 'r') as fp:
+        # Read lines
+        lines = [line.strip() for line in fp.readlines()]
+        # Eliminate blank lines and comments
+        lines = filter(lambda line : not (line == '' or line.startswith('#')), lines)
+        # Strip end-of-line comments
+        lines = map(lambda line : line.split('#')[0].strip(), lines)
+        test_files = []
+        # Take only valid files
+        for file in lines:
+            try:
+                exists(file)
+                test_files.append(file)
+            except:
+                print("  {0} cannot be opened, not added".format(file))
 
 ################################## MAIN PROGRAM BODY ###########################
 #
@@ -327,38 +345,20 @@ tg = TinyG()    # Create the TinyG Object
 
 def main():
 
+    parser = argparse.ArgumentParser(description='TinyG Tester')
+    parser.add_argument('test_files', metavar='FILE', type=str, nargs='*',
+                    help='Specific test files to run')
+
+    args = parser.parse_args()
     # Open and initialize TinyG port
     tg.init_tinyg()          # Initialize the TinyG connection.
 #    tg.init_tinyg_legacy()   # Legacy initialization code
                              # We are open and ready to rock the kitty time
 
     # Open master input file - contains a list of JSON files to process
-    os.chdir(TEST_DATA_DIR)
-    try:
-        master_fd = open(TEST_MASTER_FILE, 'r')
-    except:
-        print("Failed to open test master FILE: {0}".format(TEST_MASTER_FILE))
-        s.close()
-        sys.exit(1)
+    os.chdir(normpath(join(abspath(__file__), TEST_DATA_DIR)))
 
-    # Build a list of input files and test each one for existence
-    print("Building test set from FILE: {0}".format(TEST_MASTER_FILE))
-    temp_files = [x.strip() for x in master_fd.readlines()]     # raw file list
-    test_files = []                                             # processed list
-
-    for file in temp_files:
-        if file[:1] == '#':                 # skip over commented lines
-            continue
-
-        if file == "":                      # skip over blank lines
-            continue
-
-        file = file.split('#')[0].strip()   # strip inline comments from file text line
-        try:
-            exists(file)
-            test_files.append(file)
-        except:
-            print("  {0} cannot be opened, not added".format(file))
+    test_files  = args.test_files or get_tests_from_file(TEST_MASTER_FILE)
 
     # Iterate through the master file list to run the tests
     timestamp = time.strftime("%Y-%m%d-%H%M", time.localtime()) # e.g. 2016-0111-1414
@@ -366,18 +366,18 @@ def main():
     t_data = { "status":0 }
 
     for test_file in test_files:
-        
+
         # Display filename so opening or parsing errors are obvious
         print
         print("===========================================================")
-        
+
         # Open input file, read the file and split into 1 or more JSON objects
         try:
             in_fd = open(test_file, 'r')
         except:
             print("CANNOT OPEN: {0}".format(test_file))
             exit(1)
-            
+
         print("RUN: {0}".format(test_file))
         tests = []
         tests = split_json_file(in_fd, tests, "  ")
@@ -390,7 +390,7 @@ def main():
         after_all = ""
         after_each = ""
         params = ""
-        
+
         for obj in tests:
 
             if "before_all" in obj:
@@ -407,7 +407,7 @@ def main():
 
             if "after_each" in obj:
                 after_each = obj
-                
+
             if "defaults" in obj:
                 params = obj["defaults"]
 
@@ -424,7 +424,7 @@ def main():
 
         # Run the test or tests found in the file
         do_before_after("before_all", before_all, params)
-        
+
         for t_data in tests:
             if "t" in t_data:
                 status = run_test(t_data, before_each, after_each, params)
@@ -435,12 +435,12 @@ def main():
 
     # Close files USB port and exit
     tg.serial_close()
-    master_fd.close()
+    
     try:
         in_fd.close()
     except:
         pass
-    try:  
+    try:
         out_fd.close()
     except:
         pass
